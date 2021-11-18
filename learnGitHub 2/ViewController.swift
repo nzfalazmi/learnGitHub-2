3d2209fe0108456344136fed98c3687492c17d97
TestAuthor <exaple@gmail.com>
2021-11-18 13:41:04 +0300

    Initial Commit

---
+++ learnGitHub 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  learnGitHub 2
+//
+//  Created by Fahad Alazmi on 11/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ learnGitHub 2Tests/learnGitHub_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  learnGitHub_2Tests.swift
+//  learnGitHub 2Tests
+//
+//  Created by Fahad Alazmi on 11/18/21.
+//
+
+import XCTest
+@testable import learnGitHub_2
+
+class learnGitHub_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
